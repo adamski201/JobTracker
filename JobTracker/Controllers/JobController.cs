ee6b55cc6348921319909e00baa744aab3cc4514
Adam O'Sullivan <90043609+adamski201@users.noreply.github.com>
2023-11-11 15:12:40 +0000

    Initial commit

---
+++ JobTracker/Controllers/JobController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JobTracker.Controllers
+{
+    [ApiController]
+    public class JobController
+    {
+        private readonly ILogger logger;
+
+        public JobController(ILogger logger)
+        {
+            this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+    }
+}
