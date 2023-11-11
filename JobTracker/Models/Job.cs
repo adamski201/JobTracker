d7985d9b90df60f81d65b41ab8cd118559466cd2
Adam O'Sullivan <90043609+adamski201@users.noreply.github.com>
2023-11-11 16:33:38 +0000

    Add Job Entity

---
+++ JobTracker/Models/Job.cs
@@ -0,0 +1,12 @@
+﻿namespace JobTracker.Models
+{
+    public class Job
+    {
+        public int Id { get; set; }
+        public string EmployerName { get; set; }
+        public string JobTitle { get; set; }
+        public float Salary { get; set; }
+        public string City { get; set; }
+        public string Description { get; set; }
+    }
+}
